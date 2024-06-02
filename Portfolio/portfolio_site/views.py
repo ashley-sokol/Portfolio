05eeb02410936a1adc62f6d0f969acf6a36c0b92
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-06-02 10:25:42 -0400

    initial setup of site structure

--- Portfolio/portfolio_site/views.py
+++ Portfolio/portfolio_site/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
+class IndexView(generic.ListView):
+    template = 'portfolio_site/index.html'
+    return HttpResponse(template.render(request))
